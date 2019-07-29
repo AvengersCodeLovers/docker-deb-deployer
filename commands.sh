941410fb47d434a6f763395dd3480416e86189d7
Nguyen Khac Duy Ngoc <nguyen.khac.duy.ngoc@sun-asterisk.com>
2019-07-29 16:31:34 +0700

    add non strict hostkeyChecking

--- commands.sh
+++ commands.sh
@@ -5,6 +5,7 @@ echo -e $PLUGIN_PRIVATEKEY > /root/.ssh/id_rsa
 chmod 700 /root/.ssh
 chmod 600 /root/.ssh/id_rsa
 chmod 644 /root/.ssh/id_rsa.pub
+echo -e "Host *\n\tStrictHostKeyChecking no\n" >> /root/.ssh/config
 echo -e "Host $BASTION_SERVER\n\tUser $BASTION_USER\n\tIdentityFile /root/.ssh/id_rsa\n" >> /root/.ssh/config
 echo -e "Host $PRIVATE_SERVER\n\tUser $PRIVATE_USER\n\tIdentityFile /root/.ssh/id_rsa\n\tProxyCommand ssh $BASTION_USER@$BASTION_SERVER -W %h:%p\n" >> /root/.ssh/config
 echo "commandcollapse $PLUGIN_RUN"
