cdbafab076f2e3e2504e14fa968376e14c7e558b
Nguyen Khac Duy Ngoc <nguyen.khac.duy.ngoc@sun-asterisk.com>
2019-07-29 15:41:10 +0700

    test variable

--- commands.sh
+++ commands.sh
@@ -7,6 +7,7 @@ chmod 600 /root/.ssh/id_rsa
 chmod 644 /root/.ssh/id_rsa.pub
 echo -e "Host *\n\tStrictHostKeyChecking no\n\tIdentityFile /root/.ssh/id_rsa\n\tAddKeysToAgent yes\n" >> /root/.ssh/config
 echo -e "Host $PRIVATE_SERVER\n\tUser $PRIVATE_USER\n\tIdentityFile /root/.ssh/id_rsa\n\tProxyCommand ssh $BASTION_USER@$BASTION_SERVER -W %h:%p\n" >> /root/.ssh/config
+echo "commandcollapse $PRIVATE_SERVER"
 echo "commandcollapse $PLUGIN_RUN"
 $PLUGIN_RUN
 echo -e "deploy:\n  comment: false\n  exit_code: $?\n  ignore: false" >> ./.framgia-ci-result.temp.yml
